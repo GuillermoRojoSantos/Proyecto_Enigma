c43d9c2546a64ac766a67018cff69625b30f46fb
GuillermoRojoSantos <93922365+GuillermoRojoSantos@users.noreply.github.com>
2021-11-30 17:58:40 +0100

    Update Testeos.java

--- src/Testeos.java
+++ src/Testeos.java
@@ -1,31 +1,29 @@
 import java.util.Scanner;
 
 public class Testeos {
+    
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         String cadena=sc.nextLine();
         String nueva="";
         String nueva2="";
         int c=0;
         for (int i=0; i<cadena.length();i++) {
+            c++;
             while(c<3) {
                 nueva=nueva+cadena.charAt(i);
                 //System.out.println(nueva);
-                c++;
                 break;
             }
             while(c>=3) {
                 nueva2=nueva2+cadena.charAt(i);
                 //System.out.println(nueva2);
-                c++;
                 break;
             }
             if(c==6) {
                 c=0;
 
             }
-asd
 
 
         }
