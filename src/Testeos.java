17f6ab7c98abf693e323192dc1e1547e4a5a05b9
GuillermoRojoSantos <93922365+GuillermoRojoSantos@users.noreply.github.com>
2021-11-30 20:01:52 +0100

    Update Testeos.java

--- src/Testeos.java
+++ src/Testeos.java
@@ -1,33 +1,87 @@
 import java.util.Scanner;
+public class EnigmaMachine {
 
-public class Testeos {
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String cadena=sc.nextLine();
-        String nueva="";
-        String nueva2="";
-        int c=0;
-        for (int i=0; i<cadena.length();i++) {
-            c++;
-            while(c<3) {
-                nueva=nueva+cadena.charAt(i);
-                //System.out.println(nueva);
-                break;
-            }
-            while(c>3) {
-                nueva2=nueva2+cadena.charAt(i);
-                //System.out.println(nueva2);
-                break;
-            }
-            if(c==6) {
-                c=0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Introduzca una frase para encriptar: ");
+		String cadena= sc.nextLine();//la cadena introducida
+		System.out.println("Escoja valor del rotor: ");
+		int valor=sc.nextInt();
+		System.out.println("Elija si quiere Encriptar (E) o Desencriptar (D)");
+		char encriptOption=sc.next().charAt(0);
+		int c=0;
+		if (encriptOption=='E'|| encriptOption=='e') {
+			Encriptar(cadena, valor);
+		}
+		if (encriptOption=='D' || encriptOption=='d') {
+			Desencriptar(cadena, valor);
+		}
+	}
+	public static void Encriptar(String cadena, int valor) {
+		int c=0;
+		String nueva="";
+		for (int i=0; i<cadena.length();i++) {
+			while(c<3) {
+				nueva=nueva+Cifrardor(cadena.charAt(i), valor);
+				//System.out.println(nueva);
+				break;
+			}
+			while(c>=3) {
+				nueva=nueva+cadena.charAt(i);
+				//System.out.println(nueva2);
+				break;
+			}
+			c++;
+			if(c==6) {
+				c=0;
+			}
+		}
+		System.out.println(nueva);
+	}
+	public static void Desencriptar(String cadena, int valor) {
+		int c=0;
+		String nueva="";
+		for (int i=0; i<cadena.length();i++) {
+			while(c<3) {
+				nueva=nueva+Descifrar(cadena.charAt(i), valor);
+				//System.out.println(nueva);
+				break;
+			}
+			while(c>=3) {
+				nueva=nueva+cadena.charAt(i);
+				//System.out.println(nueva2);
+				break;
+			}
+			c++;
+			if(c==6) {
+				c=0;
 
-            }
+			}
 
 
-        }
-        System.out.println(nueva);
-        System.out.println(nueva2);
-    }
+		}
+		System.out.println(nueva);
+	}
+	public static char Cifrardor (char letra, int valor) {
+
+		int codigo=(int)letra;
+		int desplazamiento = codigo+valor%95;
+		int resultado;
+		if (desplazamiento>126) resultado=desplazamiento-95;
+		else resultado=desplazamiento;
+		return (char)resultado;
+
+
+	}
+	public static char Descifrar (char letra, int valor) {
+
+		int codigo=(int)letra;
+		int desplazamiento = codigo-valor%95;
+		int resultado;
+		if (desplazamiento<32) resultado=desplazamiento+95;
+		else resultado=desplazamiento;
+		return (char)resultado;
+
+	}
 }
