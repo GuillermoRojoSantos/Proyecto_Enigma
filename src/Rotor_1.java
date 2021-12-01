863b0f283329a9a1e13d05d06fc334c65e2f6122
GuillermoRojoSantos <guillermo.s250439@cesurformacion.com>
2021-12-01 09:59:17 +0100

    Guille

--- src/Rotor_1.java
+++ src/Rotor_1.java
@@ -1,14 +1,15 @@
+import java.util.Collections;
 import java.util.Scanner;
 public class Rotor_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
-		System.out.println("Introduzca una frase para encriptar: ");
+		System.out.println("Introduzquen eine frasen paren encriptaren or desencriptaren: ");
 		String cadena= sc.nextLine();//la cadena introducida
-		System.out.println("Escoja valor del rotor: ");
+		System.out.println("Escojen valoren das rotoren: ");
 		int valor=sc.nextInt();
-		System.out.println("Elija si quiere Encriptar (E) o Desencriptar (D)");
+		System.out.println("Elija si quieren Encriptaren (E) o Desencriptaren (D)");
 		char encriptOption=sc.next().charAt(0);
 		int c=0;
 		if (encriptOption=='E'|| encriptOption=='e') {
@@ -37,6 +38,7 @@ public class Rotor_1 {
 				c=0;
 			}
 		}
+		System.out.println("Das frasen encriptaren ist: ");
 		System.out.println(nueva);
 	}
 	public static void Desencriptar(String cadena, int valor) {
@@ -61,6 +63,7 @@ public class Rotor_1 {
 
 
 		}
+		System.out.println("Das frasen desencriptaren ist: ");
 		System.out.println(nueva);
 	}
 	public static char Cifrardor (char letra, int valor) {
@@ -75,6 +78,7 @@ public class Rotor_1 {
 
 	}
 	public static char Descifrar (char letra, int valor) {
+
 
 		int codigo=(int)letra;
 		int desplazamiento = codigo-valor%95;
